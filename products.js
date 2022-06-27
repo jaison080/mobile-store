ecdf0fb836a2d2e99f0da8d44399446b96870f29
jaison080 <jaisondennis080@gmail.com>
2022-06-27 21:55:00 +0530

    Inital commit

---
+++ products.js
@@ -0,0 +1,103 @@
+const products = [
+  {
+    id: 'price_1LFIxZSDqzOqjtR7dxTjrOCS',
+    name: 'Realme 8 5G',
+    price: 699900,
+    currency: 'INR',
+    image: '/mobiles/realme.png',
+    rating: {
+      count: 80,
+      rate: 4,
+    },
+  },
+  {
+    id: 'price_1LFK2zSDqzOqjtR7jtPgftEr',
+    name: 'Realme C21Y',
+    price: 1049900,
+    currency: 'INR',
+    image: '/mobiles/realmec21y.png',
+    rating: {
+      count: 70,
+      rate: 3,
+    },
+  },
+  {
+    id: 'price_1LFK3lSDqzOqjtR7LsuuCfpO',
+    name: 'Realme C25Y',
+    price: 699900,
+    currency: 'INR',
+    image: '/mobiles/realmec25y.png',
+    rating: {
+      count: 90,
+      rate: 3,
+    },
+  },
+  {
+    id: 'price_1LFK4KSDqzOqjtR7fYB394pg',
+    name: 'Redmi 9 Activ',
+    price: 1149900,
+    currency: 'INR',
+    image: '/mobiles/redmi9.png',
+    rating: {
+      count: 70,
+      rate: 3.5,
+    },
+  },
+  {
+    id: 'price_1LFK4lSDqzOqjtR7hhHridD9',
+    name: 'Samsung A23',
+    price: 1849900,
+    currency: 'INR',
+    image: '/mobiles/samsunga23.png',
+    rating: {
+      count: 95,
+      rate: 5,
+    },
+  },
+  {
+    id: 'price_1LFK5MSDqzOqjtR72U9VNFqg',
+    name: 'Samsung M32',
+    price: 1699900,
+    currency: 'INR',
+    image: '/mobiles/samsung.png',
+    rating: {
+      count: 60,
+      rate: 5,
+    },
+  },
+  {
+    id: 'price_1LFK5wSDqzOqjtR7jC3zdleP',
+    name: 'Oneplus Nord',
+    price: 2399000,
+    currency: 'INR',
+    image: '/mobiles/oneplusnord.png',
+    rating: {
+      count: 78,
+      rate: 4,
+    },
+  },
+  {
+    id: 'price_1LFK6KSDqzOqjtR7hOksU0IY',
+    name: 'Jio Phone Next',
+    price: 499900,
+    currency: 'INR',
+    image: '/mobiles/jiophonenext.png',
+    rating: {
+      count: 10,
+      rate: 2,
+    },
+  },
+  {
+    id: 'price_1LFK6hSDqzOqjtR7ZP9TofKT',
+    name: 'Iphone 11',
+    price: 4790000,
+    currency: 'INR',
+    image: '/mobiles/iphone11.png',
+    rating: {
+      count: 100,
+      rate: 5,
+    },
+  },
+];
+
+export default products;
